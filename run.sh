b8574ad4144e53bcde652fd891f6b7a7f241383e
Nicholas Villarreal <nvillarreal@protonmail.com>
2022-01-05 14:23:50 -0700

    Fix issue with backup.sh path

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ "${S3_S3V4}" = "yes" ]; then
     aws configure set default.s3.signature_version s3v4
 fi
 
-echo "$SCHEDULE backup.sh >> /var/log/backup.log" > crontab.txt
+echo "$SCHEDULE /backup.sh >> /var/log/backup.log" > crontab.txt
 
 # Add crontab
 /usr/bin/crontab crontab.txt
